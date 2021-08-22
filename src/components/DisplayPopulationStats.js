9a0005cb650350a899815a6f7fc420d3dff61830
sameerapk <sameerkmurthy@gmail.com>
2021-08-22 20:54:31 +0530

    Rendering all years as options

--- src/components/DisplayPopulationStats.js
+++ src/components/DisplayPopulationStats.js
@@ -1,12 +1,38 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import DisplayChart from './DisplayChart'
+import {countries} from '../countries'
 
 const DisplayPopulationStats = () => {
+    const [fetchYears,setFetchYears] = useState([])
+    const [selectedYear, setSelectedYear] = useState('')
+    
+    useEffect(()=>{
+        let fetchYears = [...new Set(countries.map(item => item.Year))]
+        setFetchYears(fetchYears) 
+    },[])
+
+    const handleSelectChange = (e) => {
+        setSelectedYear(e.target.value)
+    }
+
     return (
         <>
           <form>
-            <select name="" id="">
-
+            <select 
+            name="selectedYear" 
+            id="selectedYear"
+            value={selectedYear}
+            onChange={handleSelectChange}
+            >
+            {fetchYears && fetchYears.map((item)=>{
+                return (
+                    <option 
+                    key={item}
+                    value={item}>
+                        {item}
+                    </option> 
+                )
+            })}
             </select>
             </form>  
             <DisplayChart />
