676e2c2639fffd42d1254d486eb3e03269c271e3
sameerapk <sameerkmurthy@gmail.com>
2021-08-22 20:38:39 +0530

    initial layout for the app

---
+++ src/components/DisplayChart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DisplayChart = () => {
+    return (
+        <div>
+            Chart rendered
+        </div>
+    )
+}
+
+export default DisplayChart
---
+++ src/components/DisplayPopulationStats.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import DisplayChart from './DisplayChart'
+
+const DisplayPopulationStats = () => {
+    return (
+        <>
+          <form>
+            <select name="" id="">
+
+            </select>
+            </form>  
+            <DisplayChart />
+        </>
+    )
+}
+
+export default DisplayPopulationStats
